2c0a9f3622f1c01c93b3ce7e4e6b0a8fb74d6d4d
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-11-24 03:08:06 +0530

    added env variables

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,19 @@ import (
 	"os"
 	"testing"
 
+	"github.com/SoarinSkySagar/simple_bank/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:admin@127.0.0.1:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load env variables:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/SoarinSkySagar/simple_bank/api"
 	db "github.com/SoarinSkySagar/simple_bank/db/sqlc"
+	"github.com/SoarinSkySagar/simple_bank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:admin@127.0.0.1:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load env variables:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Cannot start server:", err)
 	}

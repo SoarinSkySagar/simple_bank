a0b7a5e4b704ab3828f37009db60409a677c575d
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-11-25 02:22:04 +0530

    added tests for listAccount and createAccount fns

--- api/account.go
+++ api/account.go
@@ -9,8 +9,8 @@ import (
 )
 
 type createAccountRequest struct {
-	Owner    string `json: "owner" binding: "required"`
-	Currency string `json: "currency" binding: "required,oneof=USD INR"`
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD INR EUR"`
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
@@ -36,7 +36,7 @@ func (server *Server) createAccount(ctx *gin.Context) {
 }
 
 type getAccountRequest struct {
-	ID int64 `uri:"id" binding: "required,min=1"`
+	ID int64 `uri:"id" binding:"required,min=1"`
 }
 
 func (server *Server) getAccount(ctx *gin.Context) {
@@ -60,8 +60,8 @@ func (server *Server) getAccount(ctx *gin.Context) {
 }
 
 type listAccountRequest struct {
-	PageID   int32 `form:"page_id" binding: "required,min=1"`
-	PageSize int32 `form:"page_size" binding: "required,min=5,max=10"`
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 }
 
 func (server *Server) listAccount(ctx *gin.Context) {
